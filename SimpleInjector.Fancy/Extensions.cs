6c1b51b27929a88ffd21f98e38d4daf4c8e74c41
geoflux <blakejason5@gmail.com>
2017-11-10 15:32:15 -0500

    initial code commit

---
+++ SimpleInjector.Fancy/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Funky.SimpleInjector
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Register a function to create an instance for type T given a consumer of type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>        
+        /// <param name="canCreateInstance">return true if instanceCreator Func is capable of returning an instance</param>
+        /// <param name="instanceCreator">return an instance of type T, based on the consumer of type T</param>
+        public static void RegisterSingleton<T>(this Container _this, Func<InjectionConsumerInfo, bool> canCreateInstance, Func<InjectionConsumerInfo, T> instanceCreator) where T : class
+        {
+            _this.Options.DependencyInjectionBehavior = new FancyInjectionBehavior<T>(_this, canCreateInstance, instanceCreator, info => info, Lifestyle.Singleton);
+        }
+        public static void Register<T>(this Container _this, Func<InjectionConsumerInfo, bool> canCreateInstance, Func<InjectionConsumerInfo, T> instanceCreator, Func<InjectionConsumerInfo, object> keyFunc, Lifestyle lifestyle) where T : class
+        {
+            _this.Options.DependencyInjectionBehavior = new FancyInjectionBehavior<T>(_this, canCreateInstance, instanceCreator, keyFunc, lifestyle);
+        }
+
+        public static void RegisterByParameterNames<T>(this Container _this, Lifestyle lifestyle, params Func<string, T>[] funcs) where T : class
+        {
+            var funcTable = getNamedFuncs(funcs);
+
+            Register(_this,
+                info => funcTable.Keys.Contains(info.Target.Parameter.Name),
+                info => funcTable[info.Target.Parameter.Name](info.Target.Parameter.Name),
+                info => info.Target.Parameter.Name,
+                lifestyle);
+        }
+
+        static Dictionary<string, Func<string, T>> getNamedFuncs<T>(params Func<string, T>[] funcs)
+        {
+            return funcs
+                .ToDictionary(f => f.Method.GetParameters().First().Name);
+        }
+    }
+}
---
+++ SimpleInjector.Fancy/FancyInjectionBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using SimpleInjector;
+using SimpleInjector.Advanced;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace Funky.SimpleInjector
+{
+    public class FancyInjectionBehavior<T> : IDependencyInjectionBehavior where T : class
+    {
+        readonly Container container;
+        readonly IDependencyInjectionBehavior innerBehavior;
+        readonly Func<InjectionConsumerInfo, bool> canResolve;
+        readonly Func<InjectionConsumerInfo, T> resolve;
+        readonly Lifestyle lifestyle;
+        readonly Dictionary<object, Registration> registrations = new Dictionary<object, Registration>();
+        readonly Func<InjectionConsumerInfo, object> keyFunc;
+        public FancyInjectionBehavior(Container container, Func<InjectionConsumerInfo, bool> canResolve, Func<InjectionConsumerInfo, T> instanceCreator, Func<InjectionConsumerInfo, object> keyFunc, Lifestyle lifestyle)
+        {
+            this.container = container;
+            this.innerBehavior = container.Options.DependencyInjectionBehavior;
+            this.canResolve = canResolve;
+            this.resolve = instanceCreator;
+            this.lifestyle = lifestyle;
+            if (keyFunc == null)
+            {
+                this.keyFunc = info => info;
+            }
+            else
+            {
+                this.keyFunc = keyFunc;
+            }
+        }
+        public InstanceProducer GetInstanceProducer(InjectionConsumerInfo consumer, bool throwOnFailure)
+        {
+            if (this.canResolve(consumer))
+            {
+                var key = keyFunc(consumer);
+                if (!registrations.ContainsKey(key))
+                {
+                    registrations[key] = lifestyle.CreateRegistration<T>(() => resolve(consumer), container);
+                }
+                var registration = registrations[key];
+                return InstanceProducer.FromExpression(
+                    typeof(T),
+                    registration.BuildExpression(),
+                    container);
+            }
+            return innerBehavior.GetInstanceProducer(consumer, throwOnFailure);
+        }
+        public void Verify(InjectionConsumerInfo consumer)
+        {
+            if (!this.canResolve(consumer))
+            {
+                innerBehavior.Verify(consumer);
+            }
+        }
+    }
+
+
+}
