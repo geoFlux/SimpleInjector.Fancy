dde8f29346cb0f84dd693d894e380db15092bc79
geoflux <blakejason5@gmail.com>
2017-11-14 22:35:11 -0500

    Added more RegisterByParameter methods

--- SimpleInjector.Fancy/Extensions.cs
+++ SimpleInjector.Fancy/Extensions.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Funky.SimpleInjector
+namespace SimpleInjector.Fancy
 {
     public static class Extensions
     {
@@ -33,6 +33,175 @@ namespace Funky.SimpleInjector
                 info => info.Target.Parameter.Name,
                 lifestyle);
         }
+        public static void RegisterByParameterNames<T1, T2>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2)
+            where T1 : class
+            where T2 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3, T4>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3, T4, T5>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4, Func<string, T5> func5)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+            where T5 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+            _this.RegisterByParameterNames(lifestyle, func5);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3, T4, T5, T6>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4, Func<string, T5> func5, Func<string, T6> func6)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+            where T5 : class
+            where T6 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+            _this.RegisterByParameterNames(lifestyle, func5);
+            _this.RegisterByParameterNames(lifestyle, func6);
+        }
+        public static void RegisterByParameterNames<T1, T2, T3, T4, T5, T6, T7>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4, Func<string, T5> func5, Func<string, T6> func6, Func<string, T7> func7)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+            where T5 : class
+            where T6 : class
+            where T7 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+            _this.RegisterByParameterNames(lifestyle, func5);
+            _this.RegisterByParameterNames(lifestyle, func6);
+            _this.RegisterByParameterNames(lifestyle, func7);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3, T4, T5, T6, T7, T8>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4, Func<string, T5> func5, Func<string, T6> func6, Func<string, T7> func7, Func<string, T8> func8)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+            where T5 : class
+            where T6 : class
+            where T7 : class
+            where T8 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+            _this.RegisterByParameterNames(lifestyle, func5);
+            _this.RegisterByParameterNames(lifestyle, func6);
+            _this.RegisterByParameterNames(lifestyle, func7);
+            _this.RegisterByParameterNames(lifestyle, func8);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4, Func<string, T5> func5, Func<string, T6> func6, Func<string, T7> func7, Func<string, T8> func8, Func<string, T9> func9)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+            where T5 : class
+            where T6 : class
+            where T7 : class
+            where T8 : class
+            where T9 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+            _this.RegisterByParameterNames(lifestyle, func5);
+            _this.RegisterByParameterNames(lifestyle, func6);
+            _this.RegisterByParameterNames(lifestyle, func7);
+            _this.RegisterByParameterNames(lifestyle, func8);
+            _this.RegisterByParameterNames(lifestyle, func9);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4, Func<string, T5> func5, Func<string, T6> func6, Func<string, T7> func7, Func<string, T8> func8, Func<string, T9> func9, Func<string, T10> func10)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+            where T5 : class
+            where T6 : class
+            where T7 : class
+            where T8 : class
+            where T9 : class
+            where T10 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+            _this.RegisterByParameterNames(lifestyle, func5);
+            _this.RegisterByParameterNames(lifestyle, func6);
+            _this.RegisterByParameterNames(lifestyle, func7);
+            _this.RegisterByParameterNames(lifestyle, func8);
+            _this.RegisterByParameterNames(lifestyle, func9);
+            _this.RegisterByParameterNames(lifestyle, func10);
+        }
+
+        public static void RegisterByParameterNames<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(this Container _this, Lifestyle lifestyle, Func<string, T1> func1, Func<string, T2> func2, Func<string, T3> func3, Func<string, T4> func4, Func<string, T5> func5, Func<string, T6> func6, Func<string, T7> func7, Func<string, T8> func8, Func<string, T9> func9, Func<string, T10> func10, Func<string, T11> func11)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+            where T5 : class
+            where T6 : class
+            where T7 : class
+            where T8 : class
+            where T9 : class
+            where T10 : class
+            where T11 : class
+        {
+            _this.RegisterByParameterNames(lifestyle, func1);
+            _this.RegisterByParameterNames(lifestyle, func2);
+            _this.RegisterByParameterNames(lifestyle, func3);
+            _this.RegisterByParameterNames(lifestyle, func4);
+            _this.RegisterByParameterNames(lifestyle, func5);
+            _this.RegisterByParameterNames(lifestyle, func6);
+            _this.RegisterByParameterNames(lifestyle, func7);
+            _this.RegisterByParameterNames(lifestyle, func8);
+            _this.RegisterByParameterNames(lifestyle, func9);
+            _this.RegisterByParameterNames(lifestyle, func10);
+            _this.RegisterByParameterNames(lifestyle, func11);
+        }
+
 
         static Dictionary<string, Func<string, T>> getNamedFuncs<T>(params Func<string, T>[] funcs)
         {
